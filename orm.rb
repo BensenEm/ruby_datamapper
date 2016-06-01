7e5b31db64abd1da4e3fc41038b0176b795783e2
BensenEm <bensen@MacBook-Pro-2.local>
2016-06-01 17:44:34 +0200

    ORM added

--- orm.rb
+++ orm.rb
@@ -1,3 +1,14 @@
-require 'data_mapper' # Lädt DataMapper ORM Code
+require 'data_mapper'
 
-DataMapper.setup(:default, 'sqlite::memory:')
+DataMapper::setup(:default, ENV.fetch('DATABASE_URL', "sqlite3:///#{Dir.pwd}/data.db"))
+
+class ContactRequest
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  property :email, String
+  property :message, Text
+end
+
+DataMapper.finalize
+ContactRequest.auto_upgrade!
